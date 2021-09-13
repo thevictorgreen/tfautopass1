af1488bc4eda1a14c12755faed8f04986ed82bd2
Victor Green <thevictorgreen@gmail.com>
2021-09-13 15:49:58 -0400

    fmt up 3

--- networking/inf_private_nacls_secgroups_rules.tf
+++ networking/inf_private_nacls_secgroups_rules.tf
@@ -114,49 +114,49 @@ resource "aws_network_acl_rule" "private_network_acl_rule_180" {
 # SECURITY GROUP RULES
 resource "aws_security_group_rule" "private_security_group_rule_100" {
   security_group_id = module.aws-vpc-us-east-1.private_security_group_id
-  type        = "ingress"
-  protocol    = "tcp"
-  from_port   = 22
-  to_port     = 22
-  cidr_blocks = ["0.0.0.0/0"]
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 22
+  to_port           = 22
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 
 resource "aws_security_group_rule" "private_security_group_rule_110" {
   security_group_id = module.aws-vpc-us-east-1.private_security_group_id
-  type        = "ingress"
-  protocol    = "tcp"
-  from_port   = 80
-  to_port     = 80
-  cidr_blocks = ["0.0.0.0/0"]
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 
 resource "aws_security_group_rule" "private_security_group_rule_120" {
   security_group_id = module.aws-vpc-us-east-1.private_security_group_id
-  type        = "ingress"
-  protocol    = "tcp"
-  from_port   = 443
-  to_port     = 443
-  cidr_blocks = ["0.0.0.0/0"]
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 
 resource "aws_security_group_rule" "private_security_group_rule_130" {
   security_group_id = module.aws-vpc-us-east-1.private_security_group_id
-  type        = "ingress"
-  protocol    = "icmp"
-  from_port   = -1
-  to_port     = -1
-  cidr_blocks = ["0.0.0.0/0"]
+  type              = "ingress"
+  protocol          = "icmp"
+  from_port         = -1
+  to_port           = -1
+  cidr_blocks       = ["0.0.0.0/0"]
 }
 
 
 resource "aws_security_group_rule" "private_security_group_rule_140" {
   security_group_id = module.aws-vpc-us-east-1.private_security_group_id
-  type        = "egress"
-  protocol    = -1
-  from_port   = 0
-  to_port     = 0
-  cidr_blocks = ["0.0.0.0/0"]
+  type              = "egress"
+  protocol          = -1
+  from_port         = 0
+  to_port           = 0
+  cidr_blocks       = ["0.0.0.0/0"]
 }
