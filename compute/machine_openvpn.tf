02e2ebb3be127d4a89903f9c2e65770e3974bb4f
Victor Green <thevictorgreen@gmail.com>
2021-09-14 09:26:36 -0400

    openvpn fmt

--- compute/machine_openvpn.tf
+++ compute/machine_openvpn.tf
@@ -1,15 +1,15 @@
 # openvpn variables
 variable "openvpn_machine_names" {
   description = "Host names for openvpn machines"
-  type = list(string)
-  default = ["openvpn000"]
+  type        = list(string)
+  default     = ["openvpn000"]
 }
 
 
 variable "openvpn_machine_subnets" {
   description = "Subnet where each host is to be provisioned"
-  type = map(string)
-  default = {
+  type        = map(string)
+  default     = {
     "openvpn000" = "management-useast1-public-us-east-1a-sn"
   }
 }
@@ -17,8 +17,8 @@ variable "openvpn_machine_subnets" {
 
 variable "openvpn_machine_azs" {
   description = "availability_zones for each host"
-  type = map(string)
-  default = {
+  type        = map(string)
+  default     = {
     "openvpn000" = "us-east-1a"
   }
 }
